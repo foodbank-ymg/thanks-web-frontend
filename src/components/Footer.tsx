e324499f6cd5819eb63d5365b366c5d76fbfdfd0
Ryota94 <Rn200494@gmail.com>
2023-03-27 22:38:05 +0900

    footercreated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
 const Footer = () => {
-  return <div className='h-[360px] bg-yellow-50'>Footer</div>
+  return <div className='h-[250px] bg-orange-900 rounded-3xl text-white text-xl'>
+    <div className="float-left ml-10 leading-10">
+            <p><span className='bg-amber-300 rounded-full pr-4 pl-4'>フードバンク山口</span></p>
+            <h1 className='text-3xl pt-4 font-bold'>特定非営利法人 フードバンク山口</h1>
+            <div className='pt-4'>
+              <p>山口市黒川1159-13</p>
+              <p>TEL: 080-3365-9640 (事務局) FAX: 083-963-4157</p>
+            </div>
+        </div>
+        <div className='float-right mr-20 pt-8 leading-8'>
+            <p>最近のお便り</p>
+            <p>これまでのお便り</p>
+            <p>ありがとうWebsiteについて</p>
+            <p>フードバンク山口公式サイト</p>
+        </div>
+  </div>
 }
 
 export default Footer
