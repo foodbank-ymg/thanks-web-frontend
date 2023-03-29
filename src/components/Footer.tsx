8214723f476fa18d921d4dd204135d2e82ef850f
Ryota94 <Rn200494@gmail.com>
2023-03-29 20:07:26 +0900

    format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
 
 const Footer = () => {
-  return <div className='h-[250px] bg-orange-900 rounded-3xl text-white text-xl'>
-    <div className="float-left ml-10 leading-10">
-            <p><span className='bg-amber-300 rounded-full pr-4 pl-4'>フードバンク山口</span></p>
-            <h1 className='text-3xl pt-4 font-bold'>特定非営利法人 フードバンク山口</h1>
-            <div className='pt-4'>
-              <p>山口市黒川1159-13</p>
-              <p>TEL: 080-3365-9640 (事務局) FAX: 083-963-4157</p>
-            </div>
+  return (
+    <div className='h-[250px] rounded-3xl bg-orange-900 text-xl text-white'>
+      <div className='float-left ml-10 leading-10'>
+        <p>
+          <span className='rounded-full bg-amber-300 px-4'>フードバンク山口</span>
+        </p>
+        <h1 className='pt-4 text-3xl font-bold'>特定非営利法人 フードバンク山口</h1>
+        <div className='pt-4'>
+          <p>山口市黒川1159-13</p>
+          <p>TEL: 080-3365-9640 (事務局) FAX: 083-963-4157</p>
         </div>
-        <div className='float-right mr-20 pt-8 leading-8'>
-            <p>最近のお便り</p>
-            <p>これまでのお便り</p>
-            <p>ありがとうWebsiteについて</p>
-            <p>フードバンク山口公式サイト</p>
-        </div>
-  </div>
+      </div>
+      <div className='float-right mr-20 pt-8 leading-8'>
+        <p>最近のお便り</p>
+        <p>これまでのお便り</p>
+        <p>ありがとうWebsiteについて</p>
+        <p>フードバンク山口公式サイト</p>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
