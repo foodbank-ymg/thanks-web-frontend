480c0de4bc010039b0984c3b157f00122274b950
KAZU0302 <i20matumura@tokuyama.kosen-ac.jp>
2023-04-09 22:07:26 +0900

    about-2

--- src/components/Topabout.tsx
+++ src/components/Topabout.tsx
@@ -4,19 +4,20 @@ import React from 'react'
 
 const Topabout = () => {
   return (
-    <div className='palt bg-amber-400'>
+    <div className='palt rounded-b-[24px] bg-amber-400 md:rounded-b-[50px]'>
       {''}
-      <div className='font-body mx-auto grid h-auto w-full max-w-[1024px] justify-items-stretch rounded-b-2xl text-center font-black text-red-900'>
-        <div className='justify-self-center p-8'>
-          <div className='relative text-4xl before:absolute before:bottom-[12.5%] before:left-0 before:-z-10 before:block before:h-[10px] before:w-full before:rounded-[10px] before:bg-white before:content-none'>
-            このサイトについて
-          </div>
+      <div className='mx-auto grid h-auto w-full max-w-full justify-items-stretch rounded-b-2xl text-center font-body font-black text-red-900'>
+        <div className='z-10 justify-self-center p-8'>
+          <div className='relative text-4xl'>このサイトについて</div>
         </div>
-        <div className='flex flex-col items-stretch md:flex-row '>
+        <div className='relative justify-self-center'>
+          <div className='absolute left-[-165px] top-[-44px] h-4 w-[330px] rounded-full border-8 border-white'></div>
+        </div>
+        <div className='flex flex-col items-stretch md:flex-row'>
           {/* 左島 */}
           <div className='md:w-1/2'>
-            <div className='top-20 left-40 p-[16px]'>
-              <div className='relative aspect-[10/6] w-full md:aspect-[1/1]'>
+            <div className='w-full p-[16px]'>
+              <div className='relative aspect-[10/6] max-md:mx-auto max-md:w-[90%] md:aspect-[1/1] md:max-w-lg md:flex-row-reverse'>
                 <Image
                   fill
                   className='hidden md:block'
@@ -34,26 +35,40 @@ const Topabout = () => {
           </div>
           {/* 右島　*/}
           <div className='m-auto w-[90%] p-7 text-center md:w-1/2 md:text-start'>
-            <div className='mb-2 w-[90%]'>
-              <p className='text-3xl '>たくさんの</p>
-              <p className='text-3xl '>&quot;ありがとう&quot;に出会い、</p>
-              <p className='text-3xl '>想いを伝えたいチカラが</p>
-              <p className='text-3xl '>あつまりました。</p>
-            </div>
-            <div className='my-2'>
-              <p className='text-left'>
-                みなさんからいただいた、たくさんの&quot;ありがとう&quot;をもっとあなたに届けたい気持ちから、フードバンク山口の活動に賛同してくださった方々が、それぞれの想いを胸にあつまり、このサイトをカタチにしました。
+            <div className='mb-2 '>
+              <p className='text-3xl leading-10 md:leading-[3rem]'>
+                たくさんの
+                <br className='md:hidden' />
+                &quot;ありがとう&quot;に出会い、
+                <br />
+                想いを伝えたいチカラが
+                <br />
+                あつまりました
               </p>
             </div>
-            <div className='mt-2'>
-              <p className='text-left text-lg'>
+
+            <div className='my-4 text-left md:w-[80%]'>
+              <p className='inline-block text-lg md:text-base'>
+                みなさんからいただいた、たくさんの&quot;ありがとう&quot;を
+                <br className='max-md:hidden' />
+                もっとあなたに届けたい気持ちから、
+                <br className='max-md:hidden' />
+                フードバンク山口の活動に賛同してくださった方々が、
+                <br className='max-md:hidden' />
+                それぞれの想いを胸にあつまり､
+                <br className='max-md:hidden' />
+                このサイトをカタチにしました。
+              </p>
+            </div>
+            <div className='mt-2 md:w-[75%]'>
+              <p className='text-left text-base'>
                 あなたにたくさんの&quot;ありがとう&quot;がお届けできていれば幸いです。
               </p>
             </div>
           </div>
         </div>
-        <button className='translate-y-4 justify-self-center rounded-full border-4 border-red-900 bg-amber-400 px-10 py-2 shadow-gray-500 hover:bg-red-900 hover:text-white'>
-          <p className=''>いきさつの物語を読む</p>
+        <button className='w-[290px] translate-y-6 justify-self-center rounded-full border-4 border-red-900 bg-amber-400 px-10 py-2 shadow-gray-500 hover:bg-red-900 hover:text-white'>
+          <p className='text-xl'>いきさつの物語を読む</p>
         </button>
       </div>
     </div>
