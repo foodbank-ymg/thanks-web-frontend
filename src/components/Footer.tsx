72e6a77ce867bb8cf1e57cf71466aef153c4b0f6
i20nobutomo <i20nobutomo@tokuyama.kosen-ac.jp>
2023-04-25 08:40:51 +0900

    画像を表示できるようにしてサイズに合わせてデザインを変えられるようにしました。

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,44 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='h-[250px] rounded-3xl bg-orange-900 text-xl text-white'>
-      <div className='float-left ml-10 leading-10'>
-        <p>
-          <span className='rounded-full bg-amber-300 px-4'>フードバンク山口</span>
-        </p>
+    <div className='flex flex-col items-center whitespace-nowrap rounded-[5vw] bg-orange-900 px-[44px] py-[32px] text-xl text-white lg:flex-row'>
+      <div className='text-center leading-10 lg:text-left'>
+        <Image src='logo_normal.svg' width='500' height='0' alt='logo' />
         <h1 className='pt-4 text-3xl font-bold'>特定非営利法人 フードバンク山口</h1>
         <div className='pt-4'>
           <p>山口市黒川1159-13</p>
-          <p>TEL: 080-3365-9640 (事務局) FAX: 083-963-4157</p>
+          <div className='flex flex-col md:flex-row'>
+            <p>TEL: 080-3365-9640(事務局)&emsp;</p>
+            <p>FAX: 083-963-4157</p>
+          </div>
         </div>
       </div>
-      <div className='float-right mr-20 pt-8 leading-8'>
-        <p>最近のお便り</p>
-        <p>これまでのお便り</p>
-        <p>ありがとうWebsiteについて</p>
-        <p>フードバンク山口公式サイト</p>
+      <div className='ml-0 flex flex-col pt-4 leading-8 lg:ml-auto'>
+        <a href='#' className=''>
+          ▶︎&ensp;最近のお便り
+        </a>
+        <a href='#' className=''>
+          ▶︎&ensp;これまでのお便り
+        </a>
+        <div className='ml-8 leading-[80px]'>
+          <a href='#' className=''>
+            ▶︎2023年&emsp;
+          </a>
+          <a href='#' className=''>
+            ▶︎2023年&emsp;
+          </a>
+          <a href='#' className=''>
+            ▶︎2023年&emsp;
+          </a>
+        </div>
+        <a href='#' className=''>
+          ▶&ensp;ありがとうWebsiteについて
+        </a>
+        <a href='#' className=''>
+          ▶︎&ensp;フードバンク山口公式サイト
+        </a>
       </div>
     </div>
   )
