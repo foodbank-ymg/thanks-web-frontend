7a1ae65a26af490dfd16bdefa38c43a84f46fde4
tetsuyanh <tetsuyanh@gmail.com>
2023-08-04 19:49:17 +0900

    wip

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,8 +1,13 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
+import Head from 'next/head'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import HeroLetter from '@/components/HeroLetter'
+import HeroPost from '@/components/HeroPost'
 import { GetApprovedPosts } from '@/data/posts'
 import { Post } from '@/types/Post'
 
@@ -21,58 +26,78 @@ const PostPage = ({ initialPost, allPosts }: Props) => {
   const [post, setPost] = useState<PostWithNeighbor>(initialPost)
 
   return (
-    <div className='container mx-auto max-w-screen-md'>
-      <div className='h-[360px] bg-gray-500'>Hero</div>
-      <div className='mx-auto  bg-white text-center font-body text-mybrown'>
-        <h1 className='break-words text-desc lg:whitespace-nowrap lg:text-mlabel'>
-          {post.subject}
-        </h1>
-        <p className='mt-8 mb-16 text-myyellow'>{post.recipientGroupName}</p>
-        <p className='mx-auto mb-16 w-full break-words'>{post.body}</p>
-        <p className='mt-8 mb-16 text-gray-400'>{post.createdAt}</p>
-        <div className='flex justify-center'>
-          <Image
-            src='/img/post-sample.webp'
-            width='200'
-            height='200'
-            alt='logo'
-            className='w-1/2'
-          />
+    <>
+      <Head>
+        <title>フードバンク山口ありがとうWeb</title>
+        <meta
+          name='description'
+          content='フードバンク山口の活動をお知らせるWebサイトです。現在開発中です。'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <main className='mx-auto max-w-[1280px]'>
+        {/* 固定ヘッダー */}
+        <div className='fixed z-50 w-full max-w-[1280px]'>
+          <Header />
         </div>
-      </div>
-      <div className='flex flex-col'>
-        <button onClick={() => router.back()}>一覧へもどる</button>
-        <button
-          onClick={() => {
-            if (allPosts && post.index > 0) {
-              router.replace(`/post/${allPosts[post.index - 1].id}`)
-              setPost(allPosts[post.index - 1])
-            }
-          }}
-          disabled={allPosts ? post.index === 0 : true}
-        >
-          前の記事
-        </button>
-        <button
-          onClick={() => {
-            if (allPosts && post.index < allPosts.length - 1) {
-              router.replace(`/post/${allPosts[post.index + 1].id}`)
-              setPost(allPosts[post.index + 1])
-            }
-          }}
-          disabled={allPosts ? post.index === allPosts?.length - 1 : true}
-        >
-          次の記事
-        </button>
-      </div>
-      {/* <div>
-        <h1>{post.subject}</h1>
-        <p>{post.createdAt}</p>
-        <p>{post.body}</p>
-        <Image src={post.images[0]} alt='' width={360} height={360} />
-      </div>
-       */}
-    </div>
+
+        <HeroPost />
+
+        {/* 投稿 */}
+        <div className='mx-auto max-w-screen-md'>
+          <div className='mx-auto rounded-b-[20px] bg-mywhite px-4 text-center font-body text-mybrown lg:px-0'>
+            <p className='text-myyellow'>{post.recipientGroupName}</p>
+            <h1 className='my-4 break-words text-mposthead lg:whitespace-nowrap lg:text-posthead'>
+              {post.subject}
+            </h1>
+            <p className=' text-mygray'>{post.createdAt}</p>
+
+            <p className='mx-auto w-full py-8 text-left lg:py-16'>{post.body}</p>
+
+            <div className='mx-auto flex h-[224px] w-[224px] justify-center lg:h-[370px] lg:w-[370px]'>
+              <Image
+                src={post.images[0]}
+                width={370}
+                height={370}
+                alt=''
+                className='object-cover'
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* 前後リンク */}
+        <div className='flex flex-col bg-myyellowpale'>
+          <button onClick={() => router.back()}>一覧へもどる</button>
+          <button
+            onClick={() => {
+              if (allPosts && post.index > 0) {
+                router.replace(`/post/${allPosts[post.index - 1].id}`)
+                setPost(allPosts[post.index - 1])
+              }
+            }}
+            disabled={allPosts ? post.index === 0 : true}
+          >
+            前の記事
+          </button>
+          <button
+            onClick={() => {
+              if (allPosts && post.index < allPosts.length - 1) {
+                router.replace(`/post/${allPosts[post.index + 1].id}`)
+                setPost(allPosts[post.index + 1])
+              }
+            }}
+            disabled={allPosts ? post.index === allPosts?.length - 1 : true}
+          >
+            次の記事
+          </button>
+        </div>
+
+        <div className='bg-myyellowpale'>
+          <Footer />
+        </div>
+      </main>
+    </>
   )
 }
 
