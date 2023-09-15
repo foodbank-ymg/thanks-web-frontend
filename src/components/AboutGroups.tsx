3d97a1e7e1da07321d3b991ee8c6ed492c152559
i20nobutomo <i20nobutomo@tokuyama.kosen-ac.jp>
2023-09-15 19:46:09 +0900

    wip

--- src/components/AboutGroups.tsx
+++ src/components/AboutGroups.tsx
@@ -6,47 +6,34 @@ const AboutGroups = () => {
     <div className='h-auto  bg-myyellowpale text-center'>
       <div className='flex flex-col items-center justify-center'>
         <div className='w-[60%]'>
-          <h1 className=''>○○○○○○○○○○</h1>
+          <h1 className=''>aaaaaaaaaa</h1>
           <div className='mt-[-20px] mb-6 rounded-full border-[16px] border-solid border-myyellow'></div>
           <p className='mb-6 break-words'>
-            ○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○
+            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
           </p>
         </div>
       </div>
-      <div className='flex justify-center'>
-        <Image
-          src='illust_heartflower_r.svg'
-          width='200'
-          height='0'
-          alt='logo'
-          className='mx-10 w-1/4'
-        />
 
-        <Image
-          src='illust_heartflower_r.svg'
-          width='200'
-          height='0'
-          alt='logo'
-          className='mx-10 w-1/4'
-        />
+      <div className='mb-10 flex flex-col justify-center text-left lg:flex-row'>
+        <div className='mx-10 lg:w-auto'>
+          <div className='flex  flex-row justify-center lg:flex-col'>
+            <Image src='/img/hero-fb-mark.svg' width='200' height='200' alt='logo' className='' />
+            <p className='ml-[200px] lg:ml-0'>aiueo。</p>
+          </div>
+        </div>
 
-        <Image
-          src='illust_heartflower_r.svg'
-          width='200'
-          height='0'
-          alt='logo'
-          className='mx-10 w-1/4'
-        />
-      </div>
-      <div className='flex justify-center text-left'>
-        <div className='mx-10 w-1/4 break-words'>
-          <p>○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○。</p>
+        <div className='mx-10 lg:w-auto'>
+          <div className='flex flex-row justify-center lg:flex-col'>
+            <Image src='/img/hero-fb-mark.svg' width='200' height='200' alt='logo' className='' />
+            <p className='ml-[200px] lg:ml-0'>aiueo。</p>
+          </div>
         </div>
-        <div className='mx-10  w-1/4 break-words'>
-          <p>○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○。</p>
-        </div>
-        <div className='mx-10  w-1/4 break-words'>
-          <p>○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○。</p>
+
+        <div className='mx-10 lg:w-auto'>
+          <div className='flex flex-row justify-center lg:flex-col'>
+            <Image src='/img/hero-fb-mark.svg' width='200' height='200' alt='logo' className='' />
+            <p className='ml-[200px] lg:ml-0'>aiueo。</p>
+          </div>
         </div>
       </div>
     </div>
