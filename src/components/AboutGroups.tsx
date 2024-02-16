da1a00529a5848b6d95475ca78d39a2b61cdead9
tetsuyanh <tetsuyanh@gmail.com>
2024-02-16 09:34:31 +0900

    make group linkable

--- src/components/AboutGroups.tsx
+++ src/components/AboutGroups.tsx
@@ -1,35 +1,58 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 import AboutHeadline from '@/components/AboutHeadline'
+import {
+  Event,
+  EventClickDigitech,
+  EventClickOfficial,
+  EventClickTokuyamaKosen,
+  event,
+} from '@/lib/gtag'
 
 type GroupsCountent = {
   image: string
   detail: string
+  url: string
+  event: Event
 }
 const GroupsCountents: GroupsCountent[] = [
   {
     image: '/img/group-tokuyama-kosen.png',
     detail:
       '情報電子工学科の学生有志3名が、LINEの機能を活用した記事投稿システムおよびWebサイト開発に挑戦。',
+    url: 'https://www.tokuyama.ac.jp/',
+    event: EventClickTokuyamaKosen,
   },
   {
     image: '/img/group-fbyamaguchi.png',
     detail:
       '食品を受け取った方の感謝の思いや日々の活動の情報などをより多くの方に発信し、さらなる支援を創出',
+    url: 'https://www.fbyamaguchi.org/',
+    event: EventClickOfficial,
   },
   {
     image: '/img/group-digitech-for-yamaguchi.png',
     detail:
       '徳山高専の学生やフードバンク山口など「デジテック for YAMAGUCHI」の会員同士による地域課題等の解決に挑戦する活動を促進',
+    url: 'https://digitech-ymg.org/',
+    event: EventClickDigitech,
   },
 ]
 
 const AboutGroups = () => {
   const readerGroupsContent = (cont: GroupsCountent) => {
     return (
-      <div className='flex flex-row items-center justify-center gap-[32px] md:flex-col'>
-        <div className='aspect-[1/1] max-w-[180px] rounded-full bg-mywhite p-[12px] drop-shadow-lg md:max-w-[300px] md:p-[24px]'>
+      <div className='flex flex-row items-center justify-center gap-[32px]  md:flex-col'>
+        <Link
+          href={cont.url}
+          target='_blank'
+          className='aspect-[1/1] max-w-[180px] rounded-full bg-mywhite p-[12px] drop-shadow-lg duration-200 hover:scale-105 md:max-w-[300px] md:p-[24px]'
+          onClick={() => {
+            event(cont.event)
+          }}
+        >
           <Image
             src={cont.image}
             width='300'
@@ -37,7 +60,7 @@ const AboutGroups = () => {
             alt='logo'
             className='w-full object-cover'
           />
-        </div>
+        </Link>
         <p className='md:ml-0 md:w-full md:py-0'>{cont.detail}</p>
       </div>
     )
