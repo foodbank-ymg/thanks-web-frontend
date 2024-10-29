494012efbdf36674f46ed8d5ce658ddaa528afe3
icho-plus <slimkt623@gmail.com>
2024-10-29 22:14:10 +0900

    確認お願いします

--- src/components/AboutNotes.tsx
+++ src/components/AboutNotes.tsx
@@ -1,25 +1,42 @@
 import FlagHeadline from '@/components/FlagHeadline'
+import SquareBulletList from '@/components/SquareBulletList'
 
 export default function AboutNotes() {
   return (
-    <div className='h-[400px] bg-white p-[88px]'>
+    <div className='h-[600px] bg-white p-[88px]'>
       <div className='mb-[32px]'>
         <FlagHeadline
           headline='みなさまからのメッセージについて'
           bgColor='bg-primary'
           textColor='text-black'
         />
-        <p>あああ</p>
+        <SquareBulletList
+          items={[
+            '皆様からのメッセージは、特定非営利活動法人市民活動さぽーとねっとがチェックをし、投稿して公開されます。',
+          ]}
+          bgColor='bg-orange-300'
+        />
       </div>
-
       <div className='mb-[32px]'>
         <FlagHeadline headline='著作権について' bgColor='bg-primary' textColor='text-black' />
-        <p>いいい</p>
+        <SquareBulletList
+          items={[
+            'サイト上のコンテンツ（テキスト、画像など）は著作権で保護されており、無断で複製、再配布、または転載することは禁止されています。',
+            'コンテンツを使用したい場合は必ず事前にご連絡いただき、当法人の許可を得てください。',
+          ]}
+          bgColor='bg-orange-300'
+        />
       </div>
-
       <div className='mb-[32px]'>
         <FlagHeadline headline='免責事項' bgColor='bg-primary' textColor='text-black' />
-        <p>ううう</p>
+        <SquareBulletList
+          items={[
+            '予告無しに内容やアドレスを変更または削除する場合がありますので、あらかじめご了承ください。',
+            '当サイトの利用は、利用者ご自身の責任において行われるものとします。当サイト上の掲載情報については、慎重に作成、管理しますが、すべての情報の正確性および完全性を保証するものではありません。あらかじめ、ご了承ください。',
+            '当サイトから入手された情報により発生した問題・障害に対して、当サイトの開発・運営者はいかなる責任も負いません。',
+          ]}
+          bgColor='bg-orange-300'
+        />
       </div>
     </div>
   )
--- src/components/AboutOwner.tsx
+++ src/components/AboutOwner.tsx
@@ -4,12 +4,22 @@ export default function AboutOwner() {
   return (
     <div className='h-[300px] bg-white p-[88px]'>
       <FlagHeadline
-        headline='防府コミュニティフリッジ運営団体'
+        headline='防府コミュニティフリッジを運営している団体'
         bgColor='bg-primary'
         textColor='text-black'
       />
-      <p>特定非営利活動法人市民活動さぽーとねっと</p>
-      <p>かくかくしかじか</p>
+      <div className='flex'>
+        <div className='mr-10 w-[650px]'>
+          <p className='my-5 text-xl'>特定非営利活動法人市民活動さぽーとねっと</p>
+          <p className='text-sm'>
+            新型コロナウイルス感染症による影響などで、困難を抱えている子育て家庭等を支えたいと、国内では岡山県から始まった「コミュニティフリッジ」は今、全国に広がりつつあります。
+          </p>
+          <p className='text-sm'>
+            防府市でも、山口県初の「みんなの公共冷蔵庫」として、令和3年11月に活動をスタートさせました。
+          </p>
+        </div>
+        <img src='#' alt='image' />
+      </div>
     </div>
   )
 }
