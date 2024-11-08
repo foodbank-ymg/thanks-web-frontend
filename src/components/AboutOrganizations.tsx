b7026c51a4a9e3fecc175882702dad49b848b88e
hiroto <h_kado.2021@icloud.com>
2024-11-08 14:27:41 +0900

    add "kadohara"

--- src/components/AboutOrganizations.tsx
+++ src/components/AboutOrganizations.tsx
@@ -1,9 +1,28 @@
 import FlagHeadline from '@/components/FlagHeadline'
+import { text } from 'stream/consumers'
+
+const organizations = [
+  {
+    organizationURL: 'a',
+    organizationText:
+      '情報電子工学科の学生融資二名がLINEの機能を活用した投稿システム・OCR技術を活用し、Webサイト開発に挑戦。',
+  },
+  {
+    organizationURL: 'a',
+    organizationText:
+      '情報電子工学科の学生融資二名がLINEの機能を活用した投稿システム・OCR技術を活用し、Webサイト開発に挑戦。',
+  },
+  {
+    organizationURL: 'a',
+    organizationText:
+      '情報電子工学科の学生融資二名がLINEの機能を活用した投稿システム・OCR技術を活用し、Webサイト開発に挑戦。',
+  },
+]
 
 export default function AboutOrganizations() {
   return (
-    <div className='h-[1000px] bg-gray-200 p-[88px] flex flex-col justify-start'>
-      <div className=' w-8/10 p-6 mb-6 space-y-6 text-left'>
+    <div className='flex flex-col justify-start bg-gray-200 p-[88px]'>
+      <div className=' mb-6 space-y-6 p-6 text-left'>
         <FlagHeadline
           headline='このサイトに関わっている団体'
           bgColor='bg-primary'
@@ -11,48 +30,32 @@ export default function AboutOrganizations() {
         />
 
         <ul>
-          <li>当サイトは、デジタル技術を活用して地域課題の解決等に取り組むコミュニティ「デジテック for</li>
-          <li>YAMAGUCHI」の饗応プロジェクトにより作成されました。徳山高専の学生が中心となって開発を行いま</li>
+          <li>
+            当サイトは、デジタル技術を活用して地域課題の解決等に取り組むコミュニティ「デジテック for
+          </li>
+          <li>
+            YAMAGUCHI」の饗応プロジェクトにより作成されました。徳山高専の学生が中心となって開発を行いま
+          </li>
           <li>した。</li>
         </ul>
       </div>
 
-      <div className='flex justify-center items-start w-8/10 p-2 '>
-        <div className='flex flex-col'>
-          <div className="flex justify-between h-1/2">
-
-            <div className="flex flex-col justify-start w-2/3 p-6 space-y-4 rounded-lg text-center">
-              <div className="border-2 border-gray-300 rounded-lg w-64 h-40 flex items-center justify-center bg-gray-200">
-                <p className="text-gray-500">画像がここに入ります</p>
-              </div>
-              <p>情報電子工学科の学生融資二名がLINEの機能を活用した投稿システム・OCR技術を活用し,Webサイト開発に挑戦。</p>
+      <div className='key ={organizations.id} flex justify-between'>
+        {organizations.map((organizations, index) => (
+          <div className='flex flex-col space-y-4 rounded-lg p-5 ' key ={index} >
+            <div className='flex h-40 w-128 items-center justify-center rounded-lg border-2 border-gray-300 bg-gray-200'>
+              <p className='text-gray-500'>画像がここに入ります</p>
             </div>
-
-            <div className="flex flex-col justify-start w-2/3 p-6 space-y-4 rounded-lg text-center">
-              <div className="border-2 border-gray-300 rounded-lg w-64 h-40 flex items-center justify-center bg-gray-200">
-                <p className="text-gray-500">画像がここに入ります</p>
-              </div>
-              <p>あああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-
-            <div className="flex flex-col justify-start w-2/3 p-6 space-y-4 rounded-lg text-center">
-              <div className="border-2 border-gray-300 rounded-lg w-64 h-40 flex items-center justify-center bg-gray-200">
-                <p className="text-gray-500">画像がここに入ります</p>
-              </div>
-              <p>徳山高専の学生や特定非営利活動法人市民活動サポートねっとなど「デジテックfor YAMAGUCHI」の会員同士による地域課題等の解決に挑戦する活動を促進</p>
-            </div>
-
-          </div>
-        </div>
-        
+            <p>{organizations.organizationText}</p>
+          </div>//画像はorganizationURLを挟んで表示させる
+        ))}
       </div>
 
-      <div className='flex justify-center items-center w-8/10 p-2'>
-        <div className='border-2 border-black rounded-xl w-3/4  h-64 p-6'>
+      <div className='flex items-center justify-center p-2'>
+        <div className='w-3/4 rounded-xl border-2  border-black p-6'>
           <p>OSSの案内</p>
         </div>
       </div>
-      
     </div>
   )
 }
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,7 +1,7 @@
 export default function Section({ children, bg }: { children: React.ReactNode; bg?: string }) {
   return (
     <div className='w-full border-t-[3px] border-black bg-primary pb-[88px]'>
-      <div className='mx-auto w-[1200px] border-x-[3px] border-b-[3px] border-black'>
+      <div className='mx-auto max-w-[1200px] border-x-[3px] border-b-[3px] border-black'>
         {children}
       </div>
     </div>
