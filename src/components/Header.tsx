425031ea15a4e3e866583d55ac781f39c45f3a7d
tetsuyanh <tetsuyanh@gmail.com>
2024-11-14 12:53:49 +0900

    fix header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,14 +7,17 @@ import { cn } from '@/lib/utils'
 
 const Header = () => {
   return (
-    <header className='h-[120px] bg-white'>
-      <div className='mx-auto flex max-w-[1200px] items-center justify-between'>
-        <div className='size-24 rounded-full bg-primary'>
-          <Link href='/'>
-            <div className='flex size-full items-center justify-center'>ロゴ</div>
+    <header className='h-[80px] bg-white'>
+      <div className='mx-auto flex max-w-[1200px] items-start justify-between'>
+        <div className='size-40 rounded-full bg-white p-6'>
+          <Link
+            href='/'
+            className='relative block size-full overflow-hidden rounded-full bg-primary'
+          >
+            <Image src='/img/logo.png' fill alt='logo' className='' />
           </Link>
         </div>
-        <ul className='flex gap-4'>
+        <ul className='flex h-[80px] items-center gap-4'>
           {menus.map((menu) => (
             <li key={menu.href} className={cn('rounded-full border-2 px-4 py-2', menu.bo_color)}>
               <Link href={menu.href} className={menu.tx_color}>
