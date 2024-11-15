dc5eb9c3663511cd8238e990057b47ceb757f75b
tetsuyanh <tetsuyanh@gmail.com>
2024-11-15 18:13:46 +0900

    fix layout

--- src/components/AboutOrganizations.tsx
+++ src/components/AboutOrganizations.tsx
@@ -1,5 +1,6 @@
-import FlagHeadline from '@/components/FlagHeadline'
 import { text } from 'stream/consumers'
+
+import FlagHeadline from '@/components/FlagHeadline'
 
 const organizations = [
   {
@@ -34,16 +35,16 @@ export default function AboutOrganizations() {
         </p>
       </div>
 
-      <div className='key ={organizations.id} flex'>
+      <ul className='flex flex-col space-y-4 rounded-lg p-5 md:flex-row'>
         {organizations.map((organizations, index) => (
-          <ul className='flex flex-col space-y-4 rounded-lg p-5 ' key={index}>
-            <div className='flex h-40 items-center justify-center rounded-lg border-2 border-gray-300 bg-gray-200'>
+          <li key={index}>
+            <div className='h-40 items-center justify-center rounded-lg border-2 border-gray-300 bg-gray-200'>
               <p className='text-gray-500'>画像がここに入ります</p>
             </div>
-            <li>{organizations.organizationText}</li>
-          </ul> //画像はorganizationURLを挟んで表示させる
+            <p>{organizations.organizationText}</p>
+          </li>
         ))}
-      </div>
+      </ul>
 
       <div className='flex items-center justify-center p-2'>
         <div className='w-3/4 rounded-xl border-2  border-black p-6'>
