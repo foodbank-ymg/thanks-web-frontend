3c7da6538e75b82f25d19975a0291d10039924dc
hiroto <h_kado.2021@icloud.com>
2024-12-19 17:43:29 +0900

    footer作業

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,16 +5,24 @@ import { menus } from '@/consts/menus'
 
 const Footer = () => {
   return (
-    <footer className='bg-primary'>
-      <div className='mx-auto flex h-[160px] max-w-screen-lg items-center justify-between'>
-        <div>ロゴと情報</div>
-        <ul>
-          {menus.map((menu, index) => (
-            <li key={index}>
-              <Link href={menu.href}>{menu.label}</Link>
-            </li>
-          ))}
-        </ul>
+    <footer className='bg-white'>
+      <div className='justify-starts relative mx-auto flex max-w-screen-lg flex-col items-center space-y-5 p-5 md:flex-row'>
+        <img src='null' className='w-32 p-4' alt='ロゴ' />
+
+        <div className='flex-col md:absolute md:right-0 md:top-1/2 md:-translate-y-1/2'>
+          <div className='flex space-x-5 md:flex-col md:space-x-0'>
+            <Link href='/provider'>&#x25B6; おくる</Link>
+            <Link href='/recipient'>&#x25B6; うけとる</Link>
+          </div>
+          <Link href='/about'>&#x25B6; このサイトについて</Link>
+        </div>
+
+        <div className='space-y-3 break-words text-center text-sm md:text-left'>
+          <p className='space-y-5 text-2xl'>防府コミュニティブリッジ</p>
+          <Link href='null'>特定非営利活動法人市民活動さぽーとねっと</Link>
+          <p className='mt-2'>〒745-0035 山口県防府市栄町1-1-17 (ルルサス防府)</p>
+          <p>TEL 0835-24-7744</p>
+        </div>
       </div>
     </footer>
   )
