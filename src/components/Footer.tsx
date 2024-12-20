0d411d681592d88f04bf35f8e184eba1b4021e44
tetsuyanh <tetsuyanh@gmail.com>
2024-12-20 18:38:48 +0900

    fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,19 +7,17 @@ import { menus } from '@/consts/menus'
 const Footer = () => {
   return (
     <footer className='bg-white'>
-      <div className='justify-starts relative mx-auto flex max-w-screen-lg flex-col items-center space-y-5 p-5 md:flex-row'>
-        <img src='null' className='w-32 p-4' alt='ロゴ' />
+      <div className='relative mx-auto flex max-w-screen-lg flex-col items-center justify-start space-y-5 p-5 md:flex-row'>
+        <Image src='/img/logo.png' width={128} height={128} className='p-4' alt='ロゴ' />
 
-
-        <ul className='flex-col md:absolute md:right-0 md:top-1/2 md:-translate-y-1/2'>
+        <ul className='flex-col gap-y-2 md:absolute md:right-0 md:top-1/2 md:-translate-y-1/2'>
           {menus.map((menu, index) => (
             <li key={index} className='flex '>
-              <img src = ''></img>
+              <Image src='/img/hcf_yajirushi.png' width={23} height={16} alt=''></Image>
               <Link href={menu.href}>{menu.label}</Link>
             </li>
           ))}
         </ul>
-
 
         <div className='space-y-3 break-words text-center text-sm md:text-left'>
           <p className='space-y-5 text-2xl'>防府コミュニティブリッジ</p>
